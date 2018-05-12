70b7da2e0818cce0cd3597dfc5ceabe6cac2d459
Brent Hamilton <brent@arthurkn.net>
2018-05-12 09:44:52 -0700

    deploy changes

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ git pull origin master
 kill -9 `cat pid` > /dev/null 2>&1
 rm pid > /dev/null 2>&1
 rm $WORK_DIR/$EXE_NAME
-go build -o $WORK_DIR/$EXE_NAME
+/usr/local/go/bin/go build -o $WORK_DIR/$EXE_NAME
 PORT=8080 BASE_URL=http://streakingapp.com nohup $WORK_DIR/$EXE_NAME > $WORK_DIR/log 2>&1 &
 echo $! > pid
